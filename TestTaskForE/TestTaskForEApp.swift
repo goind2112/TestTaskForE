7f5d964bc4124ac85433cae3b511c1bdf47584bc
goind2112 <110747378+goind2112@users.noreply.github.com>
2023-03-13 03:46:43 +0600

    Initial Commit

---
+++ TestTaskForE/TestTaskForEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskForEApp.swift
+//  TestTaskForE
+//
+//  Created by Даниил Мухсинятов on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskForEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
